8dc6bdf3f5dd0b700ab943c2cca2df3dc1ae57b5
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-01 20:01:22 -0300

    ex3

---
+++ aula3/aula3.js
@@ -0,0 +1,7 @@
+console.log('Rodrigo de "Lourdes"'); //String = Texto
+console.log("Rodrigo de 'Lourdes'"); //String
+console.log(`"Rodrigo" de 'Lourdes'`); //String
+console.log(3); //Nunber = Número
+console.log(123456); //Nunber
+console.log(3.5); //Nunber
+console.log(3 , 3.2, "Rodrigo"); //Nunber - String
