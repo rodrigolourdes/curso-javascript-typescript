7c3c5ec196b7685f031103d8a81aec5383e5110f
Rodrigo de Lourdes Silva <rodrigo.lourdes20@gmail.com>
2022-09-01 23:09:20 -0300

    aula 5

---
+++ curso-javascript/aula5/index.js
@@ -0,0 +1,10 @@
+let nome = "Ellen" // String
+
+console.log(nome, "conhece tal pessoa");
+console.log("conhece tal pessoa");
+console.log("conhece tal", nome, "pessoa");
+console.log("conhece", sobrenome, "pessoa");
+console.log(nome, "conhece tal pessoa");
+
+let sobrenome;
+nome = "Silva";
